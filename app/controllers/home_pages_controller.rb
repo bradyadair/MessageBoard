bcd4bc9c567d4baaa3c53eac6da0992d69f8d9c0
brady@decisely.com <coolguy043@hotmail.com>
2017-08-19 13:59:35 -0600

    add small signup and show

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,7 +1,4 @@
 class HomePagesController < ApplicationController
   def home
   end
-
-  def signup
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'home_pages#home'
-
-  get '/signup', to: 'home_pages#signup'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup', to:'users#new'
+  #post '/signup',  to: 'users#create'
+  resources :users
 end
